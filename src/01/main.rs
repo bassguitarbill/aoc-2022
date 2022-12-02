723de0f7f2b782ebb9fe4d9ac9ecf838057380ba
Bill Rossi <bassguitarbill@gmail.com>
2022-12-01 19:14:16 -0500

    Real programmers write fucking unit tests

--- src/01/main.rs
+++ src/01/main.rs
@@ -12,3 +12,27 @@ fn main() {
 fn group_by_elf(input: &str) -> Vec<u32> {
     input.split("\n\n").map(|elf| elf.lines().fold(0, |calories, snack| calories + snack.parse::<u32>().unwrap())).collect()
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_group_by_elf() {
+	let input =
+"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+	let calories = vec![6000, 4000, 11000, 24000, 10000];
+	assert_eq!(super::group_by_elf(input), calories);
+    }
+}
