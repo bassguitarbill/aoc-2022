693790bc85d04d32b3781f863cc749a5fb2e46c4
Bill Rossi <bassguitarbill@gmail.com>
2022-12-02 06:57:57 -0500

    Implement running a single day

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
 mod calorie_counting;
 mod rock_paper_scissors;
 
+use std::env;
 use aoc_2022::DailyProblem;
 use crate::calorie_counting::CalorieCounting;
 use crate::rock_paper_scissors::RockPaperScissors;
 
 fn main() {
-    let cc: Box<dyn DailyProblem> = Box::new(CalorieCounting{});
-    cc.print_solutions();
-    println!();
-    let rps: Box<dyn DailyProblem> = Box::new(RockPaperScissors{});
-    rps.print_solutions();
+    let problems: Vec<Box<dyn DailyProblem>> = vec![
+	Box::new(CalorieCounting{}),
+	Box::new(RockPaperScissors{}),
+	];
+
+    let args: Vec<String> = env::args().collect();
+    let args = &args[1..];
+
+    if args.is_empty() {
+	for p in problems.iter() {
+	    p.print_solutions();
+	    println!("");
+	}
+    } else {
+	let arg = args.first().unwrap().parse::<usize>();
+	match arg {
+	    Ok(num) => {
+		if num >= 1 && num <= problems.len() {
+		    problems.get(num - 1).unwrap().print_solutions();
+		} else {
+		    panic!("Invalid problem number {}", num);
+		}
+	    },
+	    Err(_) => { panic!("Arg must be numeric") }
+	}
+    }
 }
